4cc6e8116d4407c7de4e43ae27e0a4f0518fd7c7
JulianAvila15 <123836061+JulianAvila15@users.noreply.github.com>
2023-09-23 11:37:39 -0500

    Player Movement Example

---
+++ Project 1/Assets/Tiny RPG Forest/Scripts/BasicMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        float speed = 5f;
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime * speed;
+        Vector3 vertical = new Vector3(0.0f, Input.GetAxis("Vertical"), 0.0f);
+        transform.position = transform.position + vertical * Time.deltaTime * speed;
+    }
+}
