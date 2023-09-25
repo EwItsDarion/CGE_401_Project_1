d751f30e50ddb9cda7506e881e2fb7be1d719469
Arthur-P-V <apetersonveatch@gmail.com>
2023-09-25 00:12:04 -0500

    Turned Sample player into prefab for testing, added sprites and beginning of script for skillcheck mini-game

---
+++ Project 1/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject player;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector3(0, 0, -9);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = player.transform.position + offset;
+    }
+}
---
+++ Project 1/Assets/Scripts/SkillCheck.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillCheck : MonoBehaviour
+{
+    public GameObject bar;
+    public bool goingUp = true;
+    public bool stopped = false;
+    public float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bar.transform.localPosition.y >= 0.15f)
+        {
+            goingUp = false;
+        }
+        else if (bar.transform.localPosition.y <= -0.15f) {
+            goingUp = true;
+        }
+
+        if (goingUp && !stopped) {
+            bar.transform.Translate(Vector2.up * Time.deltaTime * speed);
+        }
+        if (!goingUp && !stopped) { 
+            bar.transform.Translate(Vector2.down * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            stopped = true;
+        }
+    }
+}
