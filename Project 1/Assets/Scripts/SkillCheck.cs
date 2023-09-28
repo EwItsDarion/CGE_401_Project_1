13bb6d5413650832e25ccaa14d03d0b2690e4b93
Arthur-P-V <apetersonveatch@gmail.com>
2023-09-28 16:18:25 -0500

    Moves skillcheck to UI

--- Project 1/Assets/Scripts/SkillCheck.cs
+++ Project 1/Assets/Scripts/SkillCheck.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillCheck : MonoBehaviour
 {
-    public GameObject bar;
+   // public GameObject bar;
+    public Image bar;
     public bool goingUp = true;
     public bool stopped = false;
     public float speed = 1;
     // Start is called before the first frame update
     void Start()
     {
-        bar = transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
@@ -19,13 +20,13 @@ public class SkillCheck : MonoBehaviour
     {
 
         //set upper limit of bar
-        if (bar.transform.localPosition.y >= 0.15f)
+        if (bar.transform.localPosition.y >= 50f)
         {
             goingUp = false;
         }
 
         //set lower limit of bar
-        else if (bar.transform.localPosition.y <= -0.15f) { //NOTE: localPosition is using the transform of the bar relative to the transform of the zones
+        else if (bar.transform.localPosition.y <= -50f) { //NOTE: localPosition is using the transform of the bar relative to the transform of the zones
             goingUp = true;
         }
 
@@ -36,7 +37,7 @@ public class SkillCheck : MonoBehaviour
 
         //send bar down zones sprite
         if (!goingUp && !stopped) { 
-            bar.transform.Translate(Vector2.down * Time.deltaTime * speed);
+            bar.transform.Translate(Vector2.down *Time.deltaTime * speed);
         }
 
         //space to stop the bar
