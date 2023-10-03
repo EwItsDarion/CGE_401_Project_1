dbf14c63489b8a545c340cdac86511a7ee592369
EwItsDarion <131904023+EwItsDarion@users.noreply.github.com>
2023-10-03 13:01:44 -0500

    Additional scenes
    Includes files for animated scene transition

---
+++ Project 1/Assets/Scripts/LevelChangerScript.cs
@@ -0,0 +1,32 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChangerScript : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(1);
+        }
+    
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("Fade_Out");
+    }
+
+    public void OnFadeComplete ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
